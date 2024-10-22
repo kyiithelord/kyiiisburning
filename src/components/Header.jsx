c987693ff77bd9cb6304e4b5c27d60c870b2f706
kyiithelord <thetzinkyaw9727@gmail.com>
2024-10-22 21:22:50 +0630

    Header Adjustment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,10 +21,11 @@ const Header = () => {
     <header className='fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900/0' >
         <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
             <h1>
-                <a href="" className='logo'>
-                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <a href="/" className='logo'>
+                       {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                        <path strokeLinecap="round" strokeLinejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
-                       </svg>
+                       </svg> */}
+                       <img src="./img/logo_svg.jpg" width={40} height={40} alt="kyiithelord" />
               </a>
             </h1>
 
