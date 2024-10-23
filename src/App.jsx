01b208f1c80c5e9baf9ec1844a5a5b3aff874e45
kyiithelord <thetzinkyaw9727@gmail.com>
2024-10-23 19:04:10 +0630

    Contact

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import About from './components/About'
 import Skills from './components/Skills'
 import Work from './components/Work'
 import Services from './components/Services'
+import Contact from './components/Contact'
 
 
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
         <Skills/>
         <Work/>
         <Services/>
+        <Contact/>
       </main>
     </>
   )
