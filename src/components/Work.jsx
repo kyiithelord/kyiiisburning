2348b4fe6538748c48f3f8ba3bd5503eab89bcaa
kyiithelord <thetzinkyaw9727@gmail.com>
2024-12-28 14:03:25 +0630

    work update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -42,7 +42,7 @@ const Work = () => {
     <section id='work' className='section'>
         <div className="container">
             <h2 className="headline-2 mb-8 reveal-up">
-                My portfolio highlights
+                Featured Projects
             </h2>
             <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
                 {works.map(({imgSrc,title,tags,projectLink},key) => 
