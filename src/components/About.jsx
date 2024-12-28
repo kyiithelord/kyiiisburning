c3787b87d8b29c0116acd258218b9ec49681c0ae
kyiithelord <thetzinkyaw9727@gmail.com>
2024-12-28 13:57:42 +0630

    about update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,8 +22,10 @@ const About = () => {
     <section id='about' className="section">
         <div className="container">
             <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
-                <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch] ">
-                    Welcome! I&apos;m Thet Zin Kyaw,a professional WEB Developer specializing in building responsive and dynamic web applications. I focus on delivering seamless, user-centric solutions that are both scalable and efficient. My passion lies in crafting immersive user experiences and scalable, efficient backend systems.
+                <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl">
+                    Welcome! <br /> I&apos;m Thet Zin Kyaw,a dedicated and professional Web Developer passionate about crafting responsive and dynamic web applications that truly make an impact. I specialize in creating seamless user experiences, blending elegant designs with robust backend systems to deliver solutions that are scalable, efficient, and user-centric.
+                    <br />
+                    Whether it's developing custom features, integrating APIs, or enhancing website performance, I aim to exceed expectations and contribute to the success of every project I work on. Beyond technical skills, I value collaboration, creativity, and a growth-oriented mindset, making every step of the development process meaningful.
                 </p>
 
                 <div className="flex flex-wrap items-center gap-4 md:gap-7 ">
